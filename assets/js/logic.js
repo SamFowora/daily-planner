2bc1a1b474054604a781f262ac9550ee951c91cd
Sam Fowora <sffowora@yahoo.co.uk>
2023-01-31 10:46:19 +0000

    adding the correct starter code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,12 +1,18 @@
 // create planner which contains:
     //header
         // 'work day scheduler' title
+            // -> html
         // simple descriptive subtext
+            // -> html
         // current time & date (or just date)
+            // -> html container + moment.js
         // dividing line beneath header
+            // -> css or html line break
     // planner which gets updated daily
+        // -> need planner to keep data throughout the day even when the app is closed
         // includes time slots from 9-5
         // events spaces
+        // -> check bootstrap, if not, html+css
             // colour change for past, present and future hours
             // save/lock button to be able to change event titles
         // [not required] can store information in local storage
